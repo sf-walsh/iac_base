08ff5fb0e4d27a1005dfdd21c768a02b951d85d2
[Sean Walsh] <[sf.walsh@gmail.com]>
2021-12-17 08:40:10 -0500

    fmt tf code

--- terraform/compute/variables.tf
+++ terraform/compute/variables.tf
@@ -11,8 +11,8 @@ variable "this" {
     patchgroup = "Critical"
     hostname-a = "demo-host-a"
     hostname-b = "demo-host-b"
-    ami_owner  = "01234567890"          #Owner of the private AMI being used
-    ami_value  = "amzn2-goldimage-*"  #AMI Name being shared
+    ami_owner  = "01234567890"           #Owner of the private AMI being used
+    ami_value  = "amzn2-goldimage-*"     #AMI Name being shared
     ami_id     = "ami-0ed9277fb7eb570c9" #Marketplace AMI - to use comment/uncomment ami line in main.tf
   }
 }
--- terraform/network/alb/variables.tf
+++ terraform/network/alb/variables.tf
@@ -3,11 +3,11 @@ variable "this" {
   type        = map(any)
 
   default = {
-    region  = "us-east-1"
-    lb_name = "demo-alb-dev"
-    lb_ip_a = "10.0.1.10"
-    lb_ip_b = "10.0.2.10"
-    env     = "dev"
+    region     = "us-east-1"
+    lb_name    = "demo-alb-dev"
+    lb_ip_a    = "10.0.1.10"
+    lb_ip_b    = "10.0.2.10"
+    env        = "dev"
     certdomain = "echimpdev.com"
 
 
